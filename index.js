19ab5a66e99befe83b56969e3c4a0639f6fd8737
blakeskipper <blakeskipper@gmail.com>
2022-03-15 21:20:26 -0400

    commit tree 8fa72ab0c900113a1615421d6ed671cb93fcaea5

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = (num1 * num2);
 
-// Write your code here
+
+let random = 0;
+++random;
+
+let num3 = 16;
+let num4 = 6;
+let mod = (16 % 6);
+
+let max = Math.max(1, 5, 7, 9, 5, 20);
